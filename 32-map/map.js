c600793f0c2dc9aec030b252c1844343b4df5e66
liuyingjie1130 <1410505543@qq.com>
2019-04-17 20:19:13 +0800

    echarts map 高亮全搞定

--- 32-map/map.js
+++ 32-map/map.js
@@ -1,5 +1,5 @@
 
-function() {
+$(function() {
   /* global BMap BMAP_NORMAL_MAP BMAP_HYBRID_MAP BMAP_ANIMATION_BOUNCE: true */
   var map = new BMap.Map('main');  
   var point = new BMap.Point(114.529963, 38.003679);
@@ -33,8 +33,8 @@ function() {
       + '时间：周二、周四下午<br>'
       + '地点：505 教室'
       + '</p>'
-      + '<a href="https://github.com/wangding" target="_blank">'
-      + '<img src="https://secure.gravatar.com/avatar/4e1b831efef47d65a762da5d5e89174c?s=250" alt="王顶" width="40px" height="40px" style="position: relative; top: -58px; left: 190px">'
+      + '<a href="https://github.com/liuyingjie1130" target="_blank">'
+      + '<img src="https://avatars2.githubusercontent.com/u/25972488?s=400&u=f461802c31f09ce2cfc5ea92b7be85d7a59c5642&v=4" alt="英杰在此" width="40px" height="40px" style="position: relative; top: -58px; left: 190px">'
       + '</a>'
       + '</div>', opts);
 
